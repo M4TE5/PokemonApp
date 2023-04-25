4236731896f077173a8b4e123c13789efa536f93
Matvei <m4tvei.p@gmail.com>
2023-04-25 17:27:40 +0300

    Data, Domain, Presentation layers

--- app/src/main/java/com/example/pokemonapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/pokemonapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.pokemonapp
+package com.example.pokemonapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pokemonapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
